f86a940bb3d5cf59992bc7c9a170b76f5846da2d
Thirteenth <thirteenth713@gmail.com>
2019-06-26 18:12:14 +0300

    new changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
+
+  /* Function for filter accordion start */
   $(function() {
     var Accordion = function(el, multiple) {
       this.el = el || {};
@@ -28,4 +30,55 @@ $( document ).ready(function() {
     
     var accordion = new Accordion($('.accordion_menu'), false);
   });
+  /* Function for filter accordion end */
+
+
+  
+  /* Slick slider settings start */
+
+  $('.section_similar_goods__wrapepr').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+    centerMode: true,
+    variableWidth: true,
+    arrows: true,
+    mobileFirst: true,
+    nextArrow: $('#nextSlide'),
+    prevArrow: $('#prevSlide'),
+    appendDots: $('#customDots'),
+    responsive: [
+      {
+        breakpoint: 1198,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          centerMode: true,
+          variableWidth: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  /* Slick slider settings end */
+
+
 });
